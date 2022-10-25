dfc0bc45d10b3ab43265010bbbcc029e24803c43
Lorenzo Fermi <Lorenzofermi@gmail.com>
2022-10-25 16:42:02 +0200

    Add a stateless and a stateful component

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
 import '../assets/stylesheets/application.scss';
 
-function Hello({ name }) {
+function StatelessHello({ name }) {
   return (
-    <div>
+    <h1 className="padded">
       Hello,
+      {' '}
       {name}
-    </div>
+    </h1>
   );
+}
+
+class StatefulHello extends Component {
+  constructor(props) { // be promoted into a class
+    super(props);
+    this.state = {
+      clicked: false,
+      counter: 0
+    }; // defines initial state
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    });
+  };
+
+  render() {
+    return (
+      <h1
+        className={`${this.state.clicked ? "clicked" : ""} padded`}
+        onClick={this.handleClick}
+      >
+        Hello,
+        {' '}
+        {this.props.name}
+        {' '}
+        {this.state.counter}
+      </h1>
+    );
+  }
 }
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(<Hello name=" World" />);
+  root.render(
+    <>
+      <StatefulHello name=" Stateful World" />
+      <br />
+      <br />
+      <StatelessHello name=" Stateless World" />
+    </>
+  );
 }
