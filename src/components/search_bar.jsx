6be1500b8209482c9f4f4ced13f904e4f6d18b27
Lorenzo Fermi <Lorenzofermi@gmail.com>
2022-10-26 12:12:53 +0200

    Add highlighted style to search bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { highlighted: false };
+  }
+
   handleInput = (event) => {
     const { searchFunction } = this.props;
 
     searchFunction(event.target.value);
   };
 
+  handleMouseOver = () => {
+    this.setState({ highlighted: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ highlighted: false });
+  };
+
   render() {
+    const { highlighted } = this.state;
     return (
-      <input type="text" className="form-search form-control" onChange={this.handleInput} />
+      <div className={highlighted ? "highlighted" : ""}>
+
+        <input
+          type="text"
+          className="form-search form-control"
+          onChange={this.handleInput}
+          onMouseOver={this.handleMouseOver}
+          onFocus={this.handleMouseOver}
+          onMouseLeave={this.handleMouseLeave}
+        />
+      </div>
     );
   }
 }
